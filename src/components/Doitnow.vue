7efa119c7133a62a09cb77b97ba801127c057092
Andrey C <ncix2012@gmail.com>
2022-07-28 13:04:48 +0300

    Модалка с описание очереди

--- src/components/Doitnow.vue
+++ src/components/Doitnow.vue
@@ -1,4 +1,21 @@
 <template>
+  <modal-box-description
+    v-model="shouldShowModalBox"
+    button="warning"
+    has-button
+    button-label="Понятно"
+    @confirm="shouldShowModalBox=false"
+  >
+    <p class="font-bold p-3">
+      Автоматизируйте процесс внедрения новых сотрудников или аттестуйте текущих с помощью регламентовНе отвлекайтесь на другие задачи, а работайте только с одной конкретной задачей
+    </p>
+    <p class="text-sm p-3">
+      Очередь позволит вам работать и в конце концов выполнить конкретную задачу или поручение. Вы не знаете, какая задача будет следующей, а следовательно не думаете о ней, и выполняете только ту, которая сейчас у вас перед глазами.
+    </p>
+    <p class="text-sm p-3">
+      Вам больше не нужно постоянно переключаться между разделами, чтобы разобрать новые сообщения от команды, решать, что делать с просроченными задачами и не забыть про задачи на сегодня.
+    </p>
+  </modal-box-description>
   <inspector-modal-box
     v-model="showInspector"
     button="warning"
@@ -67,6 +84,7 @@ import * as FILES from '@/store/actions/taskfiles.js'
 import * as MSG from '@/store/actions/taskmessages.js'
 import * as TASK from '@/store/actions/tasks.js'
 
+import ModalBoxDescription from '@/components/modals/ModalBoxDescription.vue'
 import InspectorModalBox from '@/components/Inspector/InspectorModalBox.vue'
 import DoitnowEmpty from '@/components/Doitnow/DoitnowEmpty.vue'
 import DoitnowTask from '@/components/Doitnow/DoitnowTask.vue'
@@ -82,7 +100,8 @@ export default {
     DoitnowSkeleton,
     DoitnowTask,
     InspectorModalBox,
-    Icon
+    Icon,
+    ModalBoxDescription
   },
   setup () {
     return {
@@ -103,7 +122,8 @@ export default {
     unsortedTasks: [],
     overdueReaded: [],
     showInspector: false,
-    tasksLoaded: false
+    tasksLoaded: false,
+    shouldShowModalBox: true
   }),
   computed: {
     tasksCount () {
