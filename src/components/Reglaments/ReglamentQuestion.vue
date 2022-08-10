c94caa5867bbe1a0b61dbc552da03a9aeff5aea9
Samarkin danya0 Danil <samarkin.danil@yandex.ru>
2022-08-10 17:16:24 +0300

    Если в вопросе регламента нет ни одного правильного ответа выводим текст с просьбой добавить хотя бы один ответ

--- src/components/Reglaments/ReglamentQuestion.vue
+++ src/components/Reglaments/ReglamentQuestion.vue
@@ -39,6 +39,12 @@
     >
       В данном вопросе более одного правильного ответа.
     </span>
+    <span
+      v-if="rightAnswersAmount(question) === 0"
+      class="flex whitespace-nowrap font-['Roboto'] text-[#7E7E80] dark:bg-gray-700 dark:text-gray-100 px-1 mb-2 rounded-lg text-[13px] font-medium"
+    >
+      Добавьте хотя бы один правильный ответ.
+    </span>
     <template
       v-for="(answer) in question.answers"
       :key="answer.uid"
