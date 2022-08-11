e99ed8fa909838bbfc6a7b9d6d7e4b1010229242
Ilya Butskikh <libut0853@gmail.com>
2022-08-11 16:51:15 +0300

    Фикс отображения видео в Chrome
    Теперь видео в хроме отображается в теге <video>, а не по ссылке на blob

--- src/views/CardFile.vue
+++ src/views/CardFile.vue
@@ -6,7 +6,8 @@ export default {
     return {
       intervalId: 0,
       text: ('Идет загрузка файла. Пожалуйста, подождите'),
-      dots: ('.')
+      dots: ('.'),
+      videoBlob: null
     }
   },
 
@@ -22,20 +23,36 @@ export default {
       const fileBlob = new Blob([resp.data], { type: type + '/' + format })
       const urlCreator = window.URL || window.webkitURL
       const fileURL = urlCreator.createObjectURL(fileBlob)
-      this.text = 'Файл был успешно загружен'
-      this.dots = '.'
-      clearInterval(this.intervalId)
-      window.location.href = fileURL
+      if (window.chrome && type === 'video') {
+        this.videoBlob = fileURL
+      } else {
+        this.text = 'Файл был успешно загружен'
+        this.dots = '.'
+        clearInterval(this.intervalId)
+        window.location.href = fileURL
+      }
     }).catch((err) => {
       this.text = err
       this.dots = '.'
+      this.videoBlob = null
       clearInterval(this.intervalId)
     })
   }
 }
 </script>
 <template>
-  <p class="text-[40px] font-[700]">
+  <p
+    v-if="!videoBlob"
+    class="text-[40px] font-[700]"
+  >
     {{ text }} {{ dots }}
   </p>
+  <video
+    v-if="$router.currentRoute.value.query.type === 'video' && videoBlob"
+    :key="videoBlob"
+    class="max-w-7xl max-h-[720px]"
+    :src="videoBlob"
+    autoplay
+    controls
+  />
 </template>
