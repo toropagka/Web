6bd80ab79d4a040b4ab51680e64d62bf65702626
Egor <egonik1337@gmail.com>
2022-08-17 10:43:21 +0300

    ограничение символов для названия проекта в очереди
    + удалил ненужную функцию которая нигде не испльзовалась

--- src/components/Doitnow/DoitnowTask.vue
+++ src/components/Doitnow/DoitnowTask.vue
@@ -216,7 +216,11 @@
             <div
               class="flex mb-2"
             >
-              <span class="text-black overflow-hidden truncate">{{ projects[task.uid_project]?.name }}</span>
+              <span class="text-black overflow-hidden truncate">{{
+                  projects[task.uid_project]?.name.length > 99
+                  ? projects[task.uid_project]?.name.split('').slice(0,100).join('') + '...'
+                  : projects[task.uid_project]?.name
+                }}</span>
             </div>
           </div>
         </div>
@@ -928,9 +932,6 @@ export default {
         _isEditing: false
       }
       this.$emit('changeValue', data)
-    },
-    print (message) {
-      alert(message)
     },
     showChat () {
       if (!this.isChatVisible) {
