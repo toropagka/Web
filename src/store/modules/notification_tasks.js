5d886f1481113ec1eeaf373c9c18e0b0920a81e0
MishaTovt <mikhail.tovt.02@mail.ru>
2022-08-18 15:17:28 +0300

    В очереди и уведомлениях отображаются регламенты, которые нельзя пройти (без теста)

--- src/store/modules/notification_tasks.js
+++ src/store/modules/notification_tasks.js
@@ -7,27 +7,76 @@ const state = {
 }
 
 const actions = {
-  [NOTIFICATION_TASKS.NOTIFICATION_TASKS_GENERATE]: ({ commit }) => {
+  [NOTIFICATION_TASKS.NOTIFICATION_TASKS_GENERATE]: ({ commit, dispatch }) => {
     const userUid = store.state.user.user.current_user_uid
     const userDep = store.state.employees.employees[userUid].uid_dep
-    const uncompletedReglaments = Object.values(store.state.reglaments.reglaments)
-      .filter((reglament) => { return (reglament.department_uid === userDep || reglament.department_uid === '') && !reglament.is_passed })
-      .map((reglament) => { return { uid: reglament.uid, name: reglament.name, notify: true } })
-    commit(NOTIFICATION_TASKS.NOTIFICATION_TASKS_SET, uncompletedReglaments)
-    commit(NOTIFICATION_TASKS.NOTIFICATION_TASKS_STATUS_SET, 'success')
+    const reglaments = Object.values(store.state.reglaments.reglaments).filter(reglament => {
+      return (reglament.department_uid === userDep ||
+        reglament.department_uid === '') &&
+        !reglament.is_passed
+    })
+    return Promise.all(
+      reglaments.map((reglament) => {
+        return new Promise((resolve, reject) => {
+          store.dispatch('REGLAMENT_REQUEST', reglament.uid)
+            .then(() => {
+              const reglamentQuestions = store.state.reglaments.reglamentQuestions
+              resolve(reglamentQuestions.length)
+            })
+            .catch((err) => {
+              reject(err)
+            })
+        }).then((result) => {
+          if (result !== 0) {
+            const notifyElem = {
+              uid: reglament.uid,
+              name: reglament.name,
+              notify: true
+            }
+            commit(NOTIFICATION_TASKS.NOTIFICATION_TASKS_PUSH, notifyElem)
+          }
+        })
+      })
+    ).then(() => {
+      commit(NOTIFICATION_TASKS.NOTIFICATION_TASKS_STATUS_SET, 'success')
+    })
   },
   [NOTIFICATION_TASKS.NOTIFICATION_TASKS_CLEAR]: ({ commit }) => {
-    commit(NOTIFICATION_TASKS.NOTIFICATION_TASKS_SET, [])
+    commit(NOTIFICATION_TASKS.NOTIFICATION_TASKS_CLEAR)
     commit(NOTIFICATION_TASKS.NOTIFICATION_TASKS_STATUS_SET, '')
+  },
+  [NOTIFICATION_TASKS.NOTIFICATION_TASKS_CHECK_QUESTIONS]: ({ commit }, reglament) => {
+    return new Promise((resolve, reject) => {
+      store.dispatch('REGLAMENT_REQUEST', reglament.uid)
+        .then(() => {
+          const reglamentQuestions = store.state.reglaments.reglamentQuestions
+          resolve(reglamentQuestions.length)
+        })
+        .catch((err) => {
+          reject(err)
+        })
+    }).then((result) => {
+      if (result !== 0) {
+        const notifyElem = {
+          uid: reglament.uid,
+          name: reglament.name,
+          notify: true
+        }
+        commit(NOTIFICATION_TASKS.NOTIFICATION_TASKS_PUSH, notifyElem)
+      }
+    })
   }
 }
 
 const mutations = {
-  [NOTIFICATION_TASKS.NOTIFICATION_TASKS_SET]: (state, resp) => {
-    state.notificationtasks = resp
+  [NOTIFICATION_TASKS.NOTIFICATION_TASKS_PUSH]: (state, resp) => {
+    state.notificationtasks.push(resp)
   },
   [NOTIFICATION_TASKS.NOTIFICATION_TASKS_STATUS_SET]: (state, resp) => {
     state.status = resp
+  },
+  [NOTIFICATION_TASKS.NOTIFICATION_TASKS_CLEAR]: (state) => {
+    state.notificationtasks = []
   }
 }
 
