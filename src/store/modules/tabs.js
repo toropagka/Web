c264659f144802c38c5f38011c361862cb5cc07d
MishaTovt <mikhail.tovt.02@mail.ru>
2022-08-19 10:25:28 +0300

    Вынес в стор переключение табов

---
+++ src/store/modules/tabs.js
@@ -0,0 +1,95 @@
+import * as TAB from '../actions/tabs'
+import { UID_TO_ACTION } from '../helpers/functions'
+import store from '@/store/index.js'
+import router from '@/router/index.js'
+
+const state = {
+
+}
+
+const getters = {
+  lastTab () {
+    return store.state.navigator.lastTab
+  },
+  storeNavigator () {
+    return store.state.navigator.navigator
+  }
+}
+
+const actions = {
+  [TAB.SWITCH_TAB]: ({ commit, getters }, tab) => {
+    if (tab.code === getters.lastTab) {
+      return
+    }
+    console.log(tab.code)
+    localStorage.setItem('lastTab', tab.code)
+    if (store.state.isPropertiesMobileExpanded) {
+      store.dispatch('asidePropertiesToggle', false)
+    }
+    switch (tab.code) {
+      case 'doitnow':
+        router.push('/doitnow')
+        break
+      case 'tasks':
+        router.push('/tasks')
+        store.dispatch(UID_TO_ACTION['901841d9-0016-491d-ad66-8ee42d2b496b'])
+        // asidemenu logic
+        store.commit('updateStackWithInitValue', {
+          name: 'Сегодня',
+          key: 'taskListSource',
+          value: { uid: '901841d9-0016-491d-ad66-8ee42d2b496b', param: new Date() },
+          typeVal: new Date(),
+          type: 'date'
+        })
+        store.commit('basic', { key: 'taskListSource', value: { uid: '901841d9-0016-491d-ad66-8ee42d2b496b', param: null } })
+        store.commit('basic', { key: 'mainSectionState', value: 'tasks' })
+        break
+      case 'directory':
+        router.push('/directory')
+        // asidemenu logic
+        store.commit('updateStackWithInitValue', {
+          name: 'Регламенты',
+          key: 'greedSource',
+          greedPath: 'reglaments',
+          value: this.storeNavigator.reglaments?.items
+        })
+        store.commit('basic', { key: 'greedSource', value: this.storeNavigator.reglaments?.items })
+        store.commit('basic', { key: 'mainSectionState', value: 'greed' })
+        store.commit('basic', { key: 'greedPath', value: 'reglaments' })
+        break
+      case 'clients':
+        router.push('/clients')
+        break
+      case 'settings':
+        router.push('/settings')
+        store.commit('updateStackWithInitValue', {
+          name: 'Аккаунт',
+          value: { uid: '9d3ba501-c173-462d-9b5f-0db97c06a026', param: new Date() },
+          typeVal: new Date(),
+          type: 'account'
+        })
+        store.commit('basic', { key: 'mainSectionState', value: 'settings' })
+        store.state.navigator.currentSettingsTab = 'account'
+        break
+    }
+    store.state.navigator.lastTab = localStorage.getItem('lastTab')
+    store.state.navigator.menu = []
+    store.state.navigator.menu.code = tab.code
+    if (tab.items) {
+      store.state.navigator.menu.push(...tab.items)
+      console.log(store.state.navigator.menu, tab.items)
+      console.log(store.state.navigator.menu)
+    }
+  }
+}
+
+const mutations = {
+
+}
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations
+}
