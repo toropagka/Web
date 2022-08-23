4779750010c0d82618c4e711c941d8abe5bb584b
yzk-jssc <egonik1337@gmail.com>
2022-08-23 14:57:32 +0300

    рефакторинг NavBarItem.vue
    из композишен в опшенс

--- src/components/NavBarItem.vue
+++ src/components/NavBarItem.vue
@@ -1,81 +1,3 @@
-<script setup>
-import { computed } from 'vue'
-
-const props = defineProps({
-  href: {
-    type: String,
-    default: null
-  },
-  to: {
-    type: String,
-    default: null
-  },
-  type: {
-    type: String,
-    default: 'flex'
-  },
-  activeColor: {
-    type: String,
-    default: 'text-blue-600'
-  },
-  hasDivider: Boolean,
-  isDesktopIconOnly: Boolean,
-  dropdown: Boolean,
-  active: Boolean
-})
-
-const is = computed(() => {
-  if (props.href) {
-    return 'a'
-  }
-
-  if (props.to) {
-    return 'router-link'
-  }
-
-  return 'div'
-})
-
-const componentClass = computed(() => {
-  const base = [
-    props.type,
-    'items-center',
-    'grow-0',
-    'shrink-0',
-    'relative',
-    'font-light',
-    'text-gray-700',
-    'cursor-pointer',
-    'hover:text-blue-500',
-    props.active ? props.activeColor : 'text-black dark:text-white dark:hover:text-gray-400'
-  ]
-
-  if (props.type === 'block') {
-    base.push('lg:flex')
-  }
-
-  if (!props.dropdown) {
-    base.push('py-2', 'px-3')
-  } else {
-    base.push('p-0', 'lg:py-2', 'lg:px-3')
-  }
-
-  if (props.hasDivider) {
-    base.push('lg:border-r', 'lg:border-gray-100', 'lg:dark:border-gray-800')
-  }
-
-  if (props.isDesktopIconOnly) {
-    base.push('lg:w-16', 'lg:justify-center')
-  }
-
-  return base
-})
-
-const activeClass = computed(() => {
-  return is.value === 'router-link' ? props.activeColor : null
-})
-</script>
-
 <template>
   <component
     :is="is"
@@ -87,3 +9,82 @@ const activeClass = computed(() => {
     <slot />
   </component>
 </template>
+
+<script>
+
+export default {
+  props: {
+    href: {
+      type: String,
+      default: null
+    },
+    to: {
+      type: String,
+      default: null
+    },
+    type: {
+      type: String,
+      default: 'flex'
+    },
+    activeColor: {
+      type: String,
+      default: 'text-blue-600'
+    },
+    hasDivider: Boolean,
+    isDesktopIconOnly: Boolean,
+    dropdown: Boolean,
+    active: Boolean
+  },
+  computed: {
+    is () {
+      if (this.href) {
+        return 'a'
+      }
+
+      if (this.to) {
+        return 'router-link'
+      }
+
+      return 'div'
+    },
+    componentClass () {
+      const base = [
+        this.type,
+        'items-center',
+        'grow-0',
+        'shrink-0',
+        'relative',
+        'font-light',
+        'text-gray-700',
+        'cursor-pointer',
+        'hover:text-blue-500',
+        this.active ? this.activeColor : 'text-black dark:text-white dark:hover:text-gray-400'
+      ]
+
+      if (this.type === 'block') {
+        base.push('lg:flex')
+      }
+
+      if (!this.dropdown) {
+        base.push('py-2', 'px-3')
+      } else {
+        base.push('p-0', 'lg:py-2', 'lg:px-3')
+      }
+
+      if (this.hasDivider) {
+        base.push('lg:border-r', 'lg:border-gray-100', 'lg:dark:border-gray-800')
+      }
+
+      if (this.isDesktopIconOnly) {
+        base.push('lg:w-16', 'lg:justify-center')
+      }
+
+      return base
+    },
+    activeClass () {
+      return this.is === 'router-link' ? this.activeColor : null
+    }
+  }
+}
+
+</script>
