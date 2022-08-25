ef218894dc6c6f6d47485ce8b72292248ce52d67
Bukharin Maxim <jojojopa@icloud.com>
2022-08-25 16:13:19 +0300

    Переделан скелетон для бокового меню

--- src/components/AsideMenu/AppSkeleton.vue
+++ src/components/AsideMenu/AppSkeleton.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="fixed flex w-full justify-between left-0">
-    <AsideMenuSkeleton class="w-[292px] h-[605px]" />
+    <MainMenuSkeleton class="w-[292px] h-[605px]" />
     <div class="w-10/12 h-[1200px] animate-pulse bg-gray-200" />
   </div>
 </template>
 <script>
-import AsideMenuSkeleton from '@/components/AsideMenu/AsideMenuSkeleton.vue'
+import MainMenuSkeleton from '@/components/AsideMenu/MainMenuSkeleton.vue'
 
 export default {
   components: {
-    AsideMenuSkeleton
+    MainMenuSkeleton
   }
 }
 </script>
