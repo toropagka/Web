55d562d0c159502f6deb5f3bf283e66cfea09e6b
Bukharin Maxim <jojojopa@icloud.com>
2022-08-25 16:02:56 +0300

    поменял местами script и template, новый скелетон загрузки приложения

---
+++ src/components/AsideMenu/AppSkeleton.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="fixed flex w-full justify-between left-0">
+    <AsideMenuSkeleton class="w-[292px] h-[605px]" />
+    <div class="w-10/12 h-[1200px] animate-pulse bg-gray-200" />
+  </div>
+</template>
+<script>
+import AsideMenuSkeleton from '@/components/AsideMenu/AsideMenuSkeleton.vue'
+
+export default {
+  components: {
+    AsideMenuSkeleton
+  }
+}
+</script>
