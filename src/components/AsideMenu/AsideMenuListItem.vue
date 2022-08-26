0c8e96125f5a7b5138a9659f9d96118e87d675f1
Maslov Dmitry <63534992+devmaslove@users.noreply.github.com>
2022-08-26 16:55:19 +0300

    fix Выделения пункта меню
    Пункт меню должен быть темнее если выделен - не верно отрабатывало

--- src/components/AsideMenu/AsideMenuListItem.vue
+++ src/components/AsideMenu/AsideMenuListItem.vue
@@ -1,7 +1,7 @@
 <template>
   <div
-    class="flex items-center h-[40px] cursor-pointer hover:bg-white hover:rounded-[10px] text-[#606061]"
-    :class="{'bg-white rounded-[10px] text-[#424242]': selected}"
+    class="flex items-center h-[40px] cursor-pointer hover:bg-white hover:rounded-[10px]"
+    :class="{'bg-white rounded-[10px] text-[#424242]': selected, 'text-[#606061]': !selected}"
   >
     <div class="flex-none flex items-center justify-center h-[30px] w-[30px] ml-[5px] mr-[8px] relative">
       <div
