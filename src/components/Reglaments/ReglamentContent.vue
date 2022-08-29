7071403847d07347581de02673f7ecabda2c2ad1
Samarkin danya0 Danil <samarkin.danil@yandex.ru>
2022-08-29 14:07:01 +0300

    Фикс бага ' не обновляется навстак по кнопке назад при просмотре регламента'

--- src/components/Reglaments/ReglamentContent.vue
+++ src/components/Reglaments/ReglamentContent.vue
@@ -389,6 +389,7 @@ export default {
       this.$store.commit('basic', { key: 'mainSectionState', value: 'greed' })
       this.$store.commit('basic', { key: 'greedPath', value: 'reglaments' })
       this.$store.commit('basic', { key: 'greedSource', value: this.$store.getters.sortedNavigator.reglaments.items })
+      this.$store.dispatch('popNavStack')
     }
   }
 }
