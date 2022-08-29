d684c547a79fa502d125f59b5d587b1c95e547a5
xxittacion <xxittacion@gmail.com>
2022-08-29 13:33:08 +0300

    вернул размер названия задачи до 14px

--- src/components/TasksListNew.vue
+++ src/components/TasksListNew.vue
@@ -133,7 +133,7 @@
               <contenteditable
                 v-model="props.node.info.name"
                 tag="div"
-                class="taskName p-0 ring-0 outline-none w-[calc(100%-26px)] overflow-x-clip break-words cursor-default text-[17px]"
+                class="taskName p-0 ring-0 outline-none w-[calc(100%-26px)] overflow-x-clip break-words cursor-default text-[14px]"
                 :contenteditable="props.node.info._isEditable"
                 placeholder="Введите название задачи"
                 :no-nl="true"
