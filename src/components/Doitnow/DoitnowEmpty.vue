40998dc36984be3e5d8be13ce6dd3d68c576002d
Bukharin Maxim <jojojopa@icloud.com>
2022-09-05 18:10:07 +0300

    фикс перехода из очереди в сегодня

--- src/components/Doitnow/DoitnowEmpty.vue
+++ src/components/Doitnow/DoitnowEmpty.vue
@@ -8,51 +8,23 @@
         src="@/assets/images/emptydoitnow.png"
         alt="Empty task image"
       >
-      <div>
+      <div class="flex flex-col items-center">
         <p
           class="text-xl text-center font-bold mt-[17px]"
         >
           Отличная работа! В Очереди пусто.<br>
           Запланируем еще дела?
         </p>
-        <div class="grid grid-cols-1">
-          <button
-            class="bg-[#FF912380] px-2 rounded-[8px] text-black text-sm mr-1 hover:bg-[#F5DEB3] w-[156px] h-[51px] mr-auto ml-auto mt-[35px]"
-            @click="goToday"
-          >
-            Запланировать
-          </button>
+        <div>
+          <router-link to="/tasks/today">
+            <button
+              class="bg-[#FF912380] px-2 rounded-[8px] text-black text-sm mr-1 hover:bg-[#F5DEB3] w-[156px] h-[51px] mr-auto ml-auto mt-[35px]"
+            >
+              Запланировать
+            </button>
+          </router-link>
         </div>
       </div>
     </div>
   </div>
 </template>
-
-<script>
-import { UID_TO_ACTION } from '@/store/helpers/functions'
-import { NAVIGATOR_SUCCESS } from '@/store/actions/navigator'
-
-export default {
-  emits: ['clickPlanning'],
-  methods: {
-    goToday: function () {
-      localStorage.setItem('lastTab', 'tasks')
-      this.$store.state.navigator.lastTab = localStorage.getItem('lastTab')
-      this.$store.commit(NAVIGATOR_SUCCESS)
-      this.$store.dispatch(UID_TO_ACTION['901841d9-0016-491d-ad66-8ee42d2b496b'])
-      // asidemenu logic
-      this.$store.commit('updateStackWithInitValue', {
-        name: 'Сегодня',
-        key: 'taskListSource',
-        value: { uid: '901841d9-0016-491d-ad66-8ee42d2b496b', param: new Date() },
-        typeVal: new Date(),
-        type: 'date'
-      })
-      this.$store.commit('basic', { key: 'taskListSource', value: { uid: '901841d9-0016-491d-ad66-8ee42d2b496b', param: null } })
-      this.$store.commit('basic', { key: 'mainSectionState', value: 'tasks' })
-    }
-  }
-}
-</script>
-
-<style scoped></style>
