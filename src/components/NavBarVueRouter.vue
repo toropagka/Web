4d6e93c239ed9d3ae89e252fe0e13de8fb158f7c
Maslov Dmitry <63534992+devmaslove@users.noreply.github.com>
2022-09-05 18:54:23 +0300

    Рефакторинг
    - Не работает в нав панели нажатие на проекты
    - Вынес отдельный компонент для одной "крошки"

--- src/components/NavBarVueRouter.vue
+++ src/components/NavBarVueRouter.vue
@@ -1,29 +1,12 @@
 <template>
-  <div class="flex">
+  <div class="flex gap-[12px]">
     <template
       v-for="(breadcrumb, index) in breadcrumbs"
       :key="breadcrumb.to"
     >
-      <router-link
-        v-if="breadcrumb.to"
-        class="font-['Roboto'] dark:bg-gray-700 dark:text-gray-100 rounded-lg text-[16px] breadcrumbs text-[#4C4C4D] font-[700]"
-        :to="breadcrumb.to"
-      >
-        {{ breadcrumb.name }}
-      </router-link>
-      <span
-        v-else
-        class="font-['Roboto'] dark:bg-gray-700 dark:text-gray-100 rounded-lg text-[16px] breadcrumbs text-[#4C4C4D] font-[700]"
-      >
-        {{ breadcrumb.name }}
-      </span>
-      <Icon
-        v-if="index !== breadcrumbs.length - 1"
-        class="ml-2.5 mr-2.5 text-gray-500"
-        :path="arrowForward.path"
-        :width="6"
-        :height="12"
-        :box="arrowForward.viewBox"
+      <NavBarBreadcrumb
+        :breadcrumb="breadcrumb"
+        :show-next="index !== breadcrumbs.length - 1"
       />
     </template>
   </div>
@@ -39,19 +22,15 @@
  */
 import { mapMutations } from 'vuex'
 
-import Icon from '@/components/Icon.vue'
-import arrowForward from '@/icons/arrow-forward.js'
+import NavBarBreadcrumb from '@/components/Navbar/NavBarBreadcrumb.vue'
 
 export default {
   components: {
-    Icon
+    NavBarBreadcrumb
   },
   computed: {
     breadcrumbs () {
       return this.$store.state.navbarVueRouter.breadcrumbs
-    },
-    arrowForward () {
-      return arrowForward
     }
   },
   mounted () {
