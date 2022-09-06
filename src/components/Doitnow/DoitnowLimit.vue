2234c2fa3bce7306e4285b39b38759561e6fae86
Samarkin danya0 Danil <samarkin.danil@yandex.ru>
2022-09-06 09:45:43 +0300

    Правки по тексту в окне лимита очереди

--- src/components/Doitnow/DoitnowLimit.vue
+++ src/components/Doitnow/DoitnowLimit.vue
@@ -6,14 +6,14 @@
       Обновите тариф
     </p>
     <p class="font-bold">
-      Очередь - функция тарифа "Бизнес" и "Альфа"
+      Очередь - функция тарифа "Бизнес+"
     </p>
     <p>С помощью данного раздела вы сможете не отвлекаться на посторонние дела и сфокусироваться на одной задаче!</p>
     <a
       href="https://www.leadertask.ru/alpha"
-      class="mt-5 w-[238px] flex justify-center items-center cursor-pointer bg-[#F2B679] text-[#2E2E2E] text-lg px-10 py-1 rounded-md hover:bg-slate-200 hover:text-[#422b14]"
+      class="mt-5 w-[238px] flex justify-center items-center text-center cursor-pointer bg-[#F2B679] text-[#2E2E2E] text-lg px-10 py-1 rounded-md hover:bg-slate-200 hover:text-[#422b14]"
     >
-      Купить
+      Подключить сейчас
     </a>
   </div>
 </template>
