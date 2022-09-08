2cdbf81a18a02ae6bb55dfd238ef40c609ec52d8
Bukharin Maxim <jojojopa@icloud.com>
2022-09-08 18:00:59 +0300

    верстка списка отделов в регламентах

--- src/components/Reglaments/ReglamentContentEditor.vue
+++ src/components/Reglaments/ReglamentContentEditor.vue
@@ -138,9 +138,30 @@
                   :key="dep.uid"
                   @click="currDep = dep.uid"
                 >
-                  <span class="truncate">
-                    {{ dep.name }}
-                  </span>
+                  <div class="flex justify-between w-full">
+                    <span
+                      class="truncate"
+                      :class="currDepTitle === dep.name ? 'font-bold' : ''"
+                    >
+                      {{ dep.name }}
+                    </span>
+                    <svg
+                      v-if="currDepTitle === dep.name"
+                      width="16"
+                      height="16"
+                      viewBox="0 0 16 16"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M13.3346 4L6.0013 11.3333L2.66797 8"
+                        stroke="#1CA345"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      />
+                    </svg>
+                  </div>
                 </PopMenuItem>
               </div>
             </template>
