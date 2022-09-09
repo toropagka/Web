16946553b72f74fabb4565dba5d5c6309e1ed0f4
Dmitry Gashilov <gashilovdmitry@yandex.ru>
2022-09-09 17:31:07 +0300

    рефакторинг shouldAddTaskIntoList

--- src/websync/utils.js
+++ src/websync/utils.js
@@ -4,194 +4,187 @@ import { computed } from 'vue'
 import { TASK_STATUS } from '@/constants'
 
 const user = computed(() => store.state.user.user)
+const settings = computed(() => store.state.navigator.navigator.settings)
 
 function isTaskIsSharedForMe (task) {
   return (task.emails.includes(user.value.current_user_email) && (task.type === 1 || task.type === 2))
 }
 
 export function shouldAddTaskIntoList (task) {
-  // Adding new task by date request
-  // first look date_end in selected day
-  //  check type 2 then check date_end < than selected date then add, if date_end > then throw away
+  if (!TaskListChecker.shouldAddCompletedTask(task)) return false
+  if (TaskListChecker.ifTaskHasParent(task)) return true
 
-  if (
-    ifTaskHasParent(task) ||
-    checkShouldAddByDateRequest(task) ||
-    checkShouldAddTaskToAssignToRequest(task) ||
-    checkShouldAddTaskToAssignByRequest(task) ||
-    checkShouldAddTaskToReady(task) ||
-    checkShouldAddTaskToProjectRequest(task) ||
-    checkShouldAddTaskToOverdueRequest(task) ||
-    checkShouldAddTaskToUnreadRequest(task) ||
-    checkShouldAddTaskToFocusRequest(task) ||
-    addToUnsortedList()
-  ) {
-    return true
+  if (typeof TaskListChecker[router.currentRoute.value.name] === 'function') {
+    return TaskListChecker[router.currentRoute.value.name](task)
   }
+
   return false
 }
 
-// Проверяет
-function checkShouldAddByDateRequest (task) {
-  // Костыльно исправляем случай, когда редактируем задачу в поиске
-  if (router.currentRoute.value.name === 'task') {
+const TaskListChecker = {
+  shouldAddCompletedTask (task) {
+    const isTaskCompleted = (task.status === TASK_STATUS.TASK_COMPLETED || task.status === TASK_STATUS.TASK_CANCELLED) ||
+        (task.type === 3 && (task.status === TASK_STATUS.TASK_READY || task.status === TASK_STATUS.TASK_REJECTED))
+    if (isTaskCompleted && !settings.value.show_completed_tasks) {
+      return false
+    }
     return true
-  }
-
-  if (!['tasksToday', 'tasksByDate'].includes(router.currentRoute.value.name)) {
-    return
-  }
-  const selectedDate = router.currentRoute.value.name === 'tasksToday' ? new Date() : new Date(router.currentRoute.value.params.date)
-  selectedDate.setHours(0, 0, 0, 0)
-  const isTaskCompleted = (task.status === TASK_STATUS.TASK_COMPLETED || task.status === TASK_STATUS.TASK_CANCELLED) ||
+  },
+  ifTaskHasParent (task) {
+    if (task.uid_parent && task.uid_parent !== '00000000-0000-0000-0000-000000000000') {
+      return true
+    }
+    return false
+  },
+  task (task) {
+    if (router.currentRoute.value.params.id === task.uid) {
+      return true
+    }
+    return false
+  },
+  search (task) {
+    const searchingText = task.name + task.comment
+    if (searchingText.toLowerCase().includes(router.currentRoute.value.query.q.toLowerCase())) {
+      return true
+    }
+    return false
+  },
+  tasksToday (task) {
+    const selectedDate = router.currentRoute.value.name === 'tasksToday' ? new Date() : new Date(router.currentRoute.value.params.date)
+    selectedDate.setHours(0, 0, 0, 0)
+    const isTaskCompleted = (task.status === TASK_STATUS.TASK_COMPLETED || task.status === TASK_STATUS.TASK_CANCELLED) ||
         (task.type === 3 && (task.status === TASK_STATUS.TASK_READY || task.status === TASK_STATUS.TASK_REJECTED))
 
-  if (
-    task.date_begin &&
-    task.date_begin !== '0001-01-01T00:00:00'
-  ) {
-    const taskDateEnd = new Date(task.date_end)
-    taskDateEnd.setHours(0, 0, 0, 0)
-    const currentDate = new Date()
-    currentDate.setHours(0, 0, 0, 0)
-
     if (
-      task.type !== 2 &&
+      task.date_begin &&
+      task.date_begin !== '0001-01-01T00:00:00'
+    ) {
+      const taskDateEnd = new Date(task.date_end)
+      taskDateEnd.setHours(0, 0, 0, 0)
+      const currentDate = new Date()
+      currentDate.setHours(0, 0, 0, 0)
+
+      if (
+        task.type !== 2 &&
       selectedDate.toLocaleString().split(',')[0] === new Date(task.date_begin).toLocaleString().split(',')[0]
-    ) {
-      return true
+      ) {
+        return true
+      } else if (
+        selectedDate <= currentDate
+      ) {
+        if (!isTaskCompleted && taskDateEnd < selectedDate) {
+          return true
+        }
+      }
     } else if (
-      selectedDate <= currentDate
+      task.customer_date_begin &&
+      task.customer_date_begin !== '0001-01-01T00:00:00' &&
+      (isTaskIsSharedForMe(task) || task.type === 3)
     ) {
-      if (!isTaskCompleted && taskDateEnd < selectedDate) {
+      const taskDateEnd = new Date(task.customer_date_end)
+      taskDateEnd.setHours(0, 0, 0, 0)
+      const currentDate = new Date()
+      currentDate.setHours(0, 0, 0, 0)
+
+      if (
+        task.type !== 2 &&
+        selectedDate.toLocaleString().split(',')[0] === new Date(task.customer_date_begin).toLocaleString().split(',')[0]
+      ) {
+        return true
+      } else if (
+        selectedDate <= currentDate
+      ) {
+        if (!isTaskCompleted && taskDateEnd < selectedDate) {
+          return true
+        }
+      }
+    }
+    return false
+  },
+  tasksByDate (task) {
+    return this.tasksToday(task)
+  },
+  tasksDelegateByMe (task) {
+    if (router.currentRoute.value.params.employee_uid === task.uid_performer) {
+      if (
+        task.email_performer &&
+        task.email_performer !== user.value.current_user_email &&
+        task.uid_customer === user.value.current_user_uid
+      ) {
         return true
       }
     }
-  } else if (
-    task.customer_date_begin &&
-    task.customer_date_begin !== '0001-01-01T00:00:00' &&
-    (isTaskIsSharedForMe(task) || task.type === 3)
-  ) {
-    const taskDateEnd = new Date(task.customer_date_end)
-    taskDateEnd.setHours(0, 0, 0, 0)
-    const currentDate = new Date()
-    currentDate.setHours(0, 0, 0, 0)
-
-    if (
-      task.type !== 2 &&
-      selectedDate.toLocaleString().split(',')[0] === new Date(task.customer_date_begin).toLocaleString().split(',')[0]
-    ) {
-      return true
-    } else if (
-      selectedDate <= currentDate
-    ) {
-      if (!isTaskCompleted && taskDateEnd < selectedDate) {
+    return false
+  },
+  tasksDelegateToMe (task) {
+    if (router.currentRoute.value.params.employee_uid === task.uid_customer) {
+      if (
+        task.email_performer &&
+        task.email_performer === user.value.current_user_email &&
+        task.uid_customer !== user.value.current_user_uid
+      ) {
         return true
       }
     }
-  }
-}
-
-// Поручено мной
-function checkShouldAddTaskToAssignToRequest (task) {
-  // Adding new task by assigned to property
-  if (
-    router.currentRoute.value.name === 'tasksDelegateByMe' &&
-    router.currentRoute.value.params.employee_uid === task.uid_performer
-  ) {
-    if (
-      task.email_performer &&
-      task.email_performer !== user.value.current_user_email &&
-      task.uid_customer === user.value.current_user_uid
-    ) {
-      return true
+    return false
+  },
+  project (task) {
+    if (router.currentRoute.value.params.project_id === task.uid_project) {
+      if (
+        task.uid_project &&
+        task.uid_project !== '00000000-0000-0000-0000-000000000000'
+      ) {
+        return true
+      }
     }
-  }
-}
-
-// Поручено мне
-function checkShouldAddTaskToAssignByRequest (task) {
-  // Adding new task by assigned by property
-  if (
-    router.currentRoute.value.name === 'tasksDelegateToMe' &&
-    router.currentRoute.value.params.employee_uid === task.uid_customer
-  ) {
+    return false
+  },
+  tasksOverdue (task) {
+    console.log(task)
+    console.log('in overdue ')
     if (
       task.email_performer &&
       task.email_performer === user.value.current_user_email &&
-      task.uid_customer !== user.value.current_user_uid
+      task.is_overdue
     ) {
       return true
     }
-  }
-}
-
-function checkShouldAddTaskToProjectRequest (task) {
-  // Добавляем задачу в список задач проекта
-  if (
-    router.currentRoute.value.name === 'project' &&
-    router.currentRoute.value.params.project_id === task.uid_project
-  ) {
-    if (
-      task.uid_project &&
-      task.uid_project !== '00000000-0000-0000-0000-000000000000'
-    ) {
-      return true
-    }
-  }
-}
-
-function checkShouldAddTaskToOverdueRequest (task) {
-  // Adding new task by overdue flag
-  if (router.currentRoute.value.name === 'tasksOverdue') {
-    if (
-      task.email_performer &&
-      task.email_performer === user.value.current_user_email &&
-      task.overdue
-    ) {
-      return true
-    }
-  }
-}
-
-function checkShouldAddTaskToReady (task) {
-  if (router.currentRoute.value.name === 'tasksReady') {
+    return false
+  },
+  tasksReady (task) {
     if (
       task.uid_customer === user.value.current_user_uid &&
       task.status === TASK_STATUS.TASK_READY
     ) {
       return true
     }
-  }
-}
-
-function checkShouldAddTaskToUnreadRequest (task) {
-  // Adding new task by unread flag
-  if (router.currentRoute.value.name === 'tasksUnread') {
+    return false
+  },
+  tasksUnread (task) {
     if (task.readed === 0) {
       return true
     }
-  }
-}
-
-function checkShouldAddTaskToFocusRequest (task) {
-  // Adding new task by focus flag
-  if (router.currentRoute.value.name === 'tasksInFocus') {
+    return false
+  },
+  tasksInFocus (task) {
     if (task.focus === 1) {
       return true
     }
-  }
-}
-
-function ifTaskHasParent (task) {
-  if (task.uid_parent && task.uid_parent !== '00000000-0000-0000-0000-000000000000') {
-    return true
-  }
-}
-
-function addToUnsortedList () {
-  // Add to unsorted list
-  if (router.currentRoute.value.name === 'taskUnsorted') {
-    return true
+    return false
+  },
+  taskUnsorted (task) {
+    if (
+      task.uid_customer === user.value.current_user_uid &&
+      task.email_performer === user.value.current_user_email &&
+      task.date_begin === '0001-01-01T00:00:00' &&
+      task.date_end === '0001-01-01T00:00:00' &&
+      task.tags.length === 0 &&
+      task.uid_marker === '00000000-0000-0000-0000-000000000000' &&
+      task.uid_project === '00000000-0000-0000-0000-000000000000' &&
+      task.focus === 0
+    ) {
+      return true
+    }
+    return false
   }
 }
