832cdf844081a27db0a89bad9aee7b79bc3fe5a0
Bukharin Maxim <jojojopa@icloud.com>
2022-09-14 17:07:47 +0300

    изменить размер шрифта пройти регламент

--- src/components/Doitnow/DoitnowNotificationTasks.vue
+++ src/components/Doitnow/DoitnowNotificationTasks.vue
@@ -7,7 +7,7 @@
         Пройти регламент "{{ name }}"
       </div>
       <button
-        class="w-xl-1/6 mt-[35px] w-[238px] h-[40px] justify-center cursor-pointer bg-[#F2B679] text-[#2E2E2E] text-lg px-10 rounded-md hover:bg-slate-200  font-medium"
+        class="w-xl-1/6 mt-[35px] w-[238px] h-[40px] justify-center cursor-pointer bg-[#F2B679] text-[#2E2E2E] text-[14px] px-10 rounded-md hover:bg-slate-200  font-medium"
         @click="gotoReglamentContent"
       >
         Пройти регламент
